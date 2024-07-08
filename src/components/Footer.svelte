7e80037599b2e65ae178b208dc8ad9b0b5a3c24d
Mohammad Taiham <mohammadtaiham.dryboss@gmail.com>
2024-07-09 00:11:29 +0600

    header footer

---
+++ src/components/Footer.svelte
@@ -0,0 +1,12 @@
+<footer>
+  <p>Copyright 2024 PollHub</p>
+</footer>
+
+<style>
+  footer {
+    padding: 20px;
+    color: #808080;
+    background-color: #292929;
+    text-align: center;
+  }
+</style>
---
+++ src/components/Header.svelte
@@ -0,0 +1,18 @@
+<header>
+  <h1>Poll<span>Hub</span></h1>
+</header>
+
+<style>
+  h1 {
+    text-align: center;
+    padding: 10px 20px;
+    margin: 20px;
+    letter-spacing: 2.5px;
+  }
+  h1 span {
+    padding: 4px;
+    color: #1b1b1b;
+    background-color: #ffa31a;
+    border-radius: 5px;
+  }
+</style>
